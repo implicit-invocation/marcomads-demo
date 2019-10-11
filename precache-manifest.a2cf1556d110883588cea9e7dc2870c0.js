ef7aa684d3b3afb63081552ffbc2856e4715a6d1
Duong Dat <tao@cu8-3.local>
2019-10-11 11:41:51 +0700

    done

---
+++ precache-manifest.a2cf1556d110883588cea9e7dc2870c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "138fc02811f585512a2e2591bb39a5fa",
+    "url": "/marcomads-demo/index.html"
+  },
+  {
+    "revision": "5979d02edf2a3af6d76e",
+    "url": "/marcomads-demo/static/css/main.3d9d8fd5.chunk.css"
+  },
+  {
+    "revision": "7082a204c2f557f53ee4",
+    "url": "/marcomads-demo/static/js/2.3bbf8cd4.chunk.js"
+  },
+  {
+    "revision": "5979d02edf2a3af6d76e",
+    "url": "/marcomads-demo/static/js/main.6abf091b.chunk.js"
+  },
+  {
+    "revision": "b7cbd081ec73f835d3a0",
+    "url": "/marcomads-demo/static/js/runtime-main.95b9a10b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb5d4d4bcb236739aa713d0c22dba99a.js"
+  "/marcomads-demo/precache-manifest.a2cf1556d110883588cea9e7dc2870c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marcomads-demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
